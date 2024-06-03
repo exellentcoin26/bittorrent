e80b6354ee7e25fb4b25558ab01aa73afe566697
exellentcoin26 <60435960+exellentcoin26@users.noreply.github.com>
2024-06-03 17:48:45 +0200

    chore: Check in nix flake

---
+++ flake.nix
@@ -0,0 +1,37 @@
+{
+  description = "CodeCrafters BitTorrent rust flake";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/release-23.11";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    flake-utils.follows = "rust-overlay/flake-utils";
+  };
+
+  outputs = {
+    nixpkgs,
+    rust-overlay,
+    flake-utils,
+    ...
+  } @ inputs:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        overlays = [(import rust-overlay)];
+        pkgs = import nixpkgs {
+          inherit system overlays;
+        };
+      in {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            rust-bin.stable."1.77.2".default
+            bacon
+
+            pkg-config
+            openssl
+          ];
+        };
+      }
+    );
+}
